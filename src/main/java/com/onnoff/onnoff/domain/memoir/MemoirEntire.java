05d09b4bc074b6dc3276ec9786466420956de93e
kimjm9841 <kimjm9841@naver.com>
2024-01-12 03:19:37 +0900

    feat: 통합 회고 엔티티 작성

---
+++ src/main/java/com/onnoff/onnoff/domain/memoir/MemoirEntire.java
@@ -0,0 +1,28 @@
+package com.onnoff.onnoff.domain.memoir;
+
+import com.onnoff.onnoff.domain.common.BaseEntity;
+import com.onnoff.onnoff.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemoirEntire extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String icon;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean bookmarked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
