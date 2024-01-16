a278a8715c0eca02433ec439f178259801006d1a
kimjm9841 <kimjm9841@naver.com>
2024-01-17 02:36:55 +0900

    feat: 회고 관련 서비스 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/off/memoir/service/MemoirService.java
@@ -0,0 +1,4 @@
+package com.onnoff.onnoff.domain.off.memoir.service;
+
+public interface MemoirService {
+}
---
+++ src/main/java/com/onnoff/onnoff/domain/off/memoir/service/MemoirServiceImpl.java
@@ -0,0 +1,9 @@
+package com.onnoff.onnoff.domain.off.memoir.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemoirServiceImpl implements MemoirService {
+}
