18b24198b76e83b78d45f0af6ace208da04d85c1
kimjm9841 <kimjm9841@naver.com>
2024-01-17 02:41:21 +0900

    feat: 회고 관련 컨트롤러 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/off/memoir/controller/MemoirController.java
@@ -0,0 +1,9 @@
+package com.onnoff.onnoff.domain.off.memoir.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemoirController {
+}
