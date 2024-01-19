62468e076c7493c239f1bba4d89a932c90fde7a6
wuseong <jws001228@naver.com>
2024-01-19 16:15:27 +0900

    feat: 유저 응답 DTO 생성, 컨버터에도 추가

--- src/main/java/com/onnoff/onnoff/domain/user/converter/UserConverter.java
+++ src/main/java/com/onnoff/onnoff/domain/user/converter/UserConverter.java
@@ -1,7 +1,8 @@
 package com.onnoff.onnoff.domain.user.converter;
 
-import com.onnoff.onnoff.auth.client.dto.KakaoOauth2DTO;
+import com.onnoff.onnoff.auth.feignClient.dto.KakaoOauth2DTO;
 import com.onnoff.onnoff.domain.user.User;
+import com.onnoff.onnoff.domain.user.dto.UserResponseDTO;
 
 public class UserConverter {
     public static User toUser(KakaoOauth2DTO.UserInfoResponseDTO response){
@@ -10,6 +11,33 @@ public class UserConverter {
                 .oauthId(response.getId())
                 .email(kakaoAccount.getEmail())
                 .name(kakaoAccount.getName())
+                .build();
+    }
+
+    public static UserResponseDTO.LoginDTO toLoginDTO(User user){
+        return UserResponseDTO.LoginDTO.builder()
+                .id(user.getId())
+                .infoSet(user.isInfoSet())
+                .createdAt(user.getCreatedAt())
+                .build();
+
+    }
+    public static UserResponseDTO.UserDetailDTO toUserDetailDTO(User user){
+        return UserResponseDTO.UserDetailDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .socialType(user.getSocialType())
+                .nickname(user.getNickname())
+                .fieldOfWork(user.getFieldOfWork())
+                .job(user.getJob())
+                .experienceYear(user.getExperienceYear())
+                .infoSet(user.isInfoSet())
+                .status(user.getStatus())
+                .inactiveDate(user.getInactiveDate())
+                .receivePushNotification(user.isReceivePushNotification())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
                 .build();
     }
 }
