a32f1cbb34d2ba0f4138e6aeb34889bf995dfd10
wuseong <jws001228@naver.com>
2024-01-19 15:54:06 +0900

    feat: ExceptionAdvice 범위에 @Controller도 추가

--- src/main/java/com/onnoff/onnoff/apiPayload/exception/ExceptionAdvice.java
+++ src/main/java/com/onnoff/onnoff/apiPayload/exception/ExceptionAdvice.java
@@ -6,10 +6,12 @@ import com.onnoff.onnoff.apiPayload.code.status.ErrorStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
-@RestControllerAdvice(annotations = {RestController.class})
+@RestControllerAdvice(annotations = {RestController.class, Controller.class})
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
