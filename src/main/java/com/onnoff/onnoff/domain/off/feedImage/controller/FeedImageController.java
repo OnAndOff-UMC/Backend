e4dc143e0484f5291cf3338b2210eb0de621d5bc
kimjm9841 <kimjm9841@naver.com>
2024-01-20 02:19:27 +0900

    feat: 피드 사진 관련 컨트롤러 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/off/feedImage/controller/FeedImageController.java
@@ -0,0 +1,9 @@
+package com.onnoff.onnoff.domain.off.feedImage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FeedImageController {
+}
