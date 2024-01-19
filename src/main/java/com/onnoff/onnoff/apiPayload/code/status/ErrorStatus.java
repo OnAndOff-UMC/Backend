8bc0991dab8ca69ac49f5c10bbc10a2d5c4dd189
wuseong <jws001228@naver.com>
2024-01-19 15:53:13 +0900

    feat: ErrorStatus 로그인 관련 에러 추가

--- src/main/java/com/onnoff/onnoff/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/onnoff/onnoff/apiPayload/code/status/ErrorStatus.java
@@ -27,7 +27,12 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 회고 답변 관련 에러
     ANSWER_NOT_FOUND(HttpStatus.BAD_REQUEST, "ANSWER4001", "해당하는 회고 답변이 없습니다."),
-    ANSWER_BAD_MATCH(HttpStatus.BAD_REQUEST, "ANSWER4002", "해당하는 회고에 속하는 회고 답변이 아닙니다.");
+    ANSWER_BAD_MATCH(HttpStatus.BAD_REQUEST, "ANSWER4002", "해당하는 회고에 속하는 회고 답변이 아닙니다."),
+
+    // 로그인 관련 에러
+    INVALID_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "LOGIN4001", "토큰값이 잘못되었거나 유효하지 않습니다."),
+    INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "LOGIN4002", "필수인자가 포함되지 않거나 인자값의 데이터 타입 또는 범위가 적절하지 않습니다."),
+    TEMPORARY_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR , "LOGIN5000", "카카오 플랫폼 서비스의 일시적 내부 장애입니다. 잠시후에 다시 시도해주세요");
 
     private final HttpStatus httpStatus;
     private final String code;
