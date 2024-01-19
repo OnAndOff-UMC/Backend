2cda095384b35a1ee7cf6fff91b0f627a6c3d3a4
kimjm9841 <kimjm9841@naver.com>
2024-01-20 02:38:29 +0900

    feat: 피드 사진 관련 리포지토리 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/off/feedImage/repository/FeedImageRepository.java
@@ -0,0 +1,7 @@
+package com.onnoff.onnoff.domain.off.feedImage.repository;
+
+import com.onnoff.onnoff.domain.off.feedImage.entity.FeedImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedImageRepository extends JpaRepository<FeedImage, Long> {
+}
