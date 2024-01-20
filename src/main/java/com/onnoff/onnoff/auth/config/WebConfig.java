5376beb1d2fda206fe33355fce05f5355baa0a4e
wuseong <jws001228@naver.com>
2024-01-20 23:25:52 +0900

    feat: 인터셉터 등록 및 디폴트 OEIV 순서 변경
    유저 인터셉터에서 JPA 사용하려면 영속성 컨텍스트 열려 있어야해서 순서 조정

--- src/main/java/com/onnoff/onnoff/auth/config/WebConfig.java
+++ src/main/java/com/onnoff/onnoff/auth/config/WebConfig.java
@@ -2,22 +2,44 @@ package com.onnoff.onnoff.auth.config;
 
 
 import com.onnoff.onnoff.auth.jwt.filter.JwtAuthFilter;
+import com.onnoff.onnoff.auth.jwt.filter.UserInterceptor;
 import com.onnoff.onnoff.auth.jwt.service.JwtTokenProvider;
+import com.onnoff.onnoff.auth.jwt.service.JwtUtil;
+import com.onnoff.onnoff.domain.user.service.UserService;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+    private final EntityManagerFactory entityManagerFactory;
     @Bean
     public FilterRegistrationBean<JwtAuthFilter> jwtFilterRegistration() {
         FilterRegistrationBean<JwtAuthFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(new JwtAuthFilter(jwtTokenProvider)); // 필터 인스턴스 설정
-        registration.addUrlPatterns("/*");
-        registration.setOrder(1); // 필터의 순서 설정. 값이 낮을수록 먼저 실행됨
+        registration.addUrlPatterns("/**");
+        registration.setOrder(1); // 필터의 순서 설정. 값이 낮을수록 먼저 실행
         return registration;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor = new OpenEntityManagerInViewInterceptor();
+        openEntityManagerInViewInterceptor.setEntityManagerFactory(entityManagerFactory);
+        registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor);
+
+        registry.addInterceptor(new UserInterceptor(userService, jwtUtil))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**", "/oauth2/**");
+
     }
 }
