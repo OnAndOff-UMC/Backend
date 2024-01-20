effd0e7f4fd942d8c903b29a483fb4d6cdd19f9a
kimjm9841 <kimjm9841@naver.com>
2024-01-21 00:21:26 +0900

    feat: 입력 형식을 multipart/form-data으로 지정

--- src/main/java/com/onnoff/onnoff/domain/off/feedImage/controller/FeedImageController.java
+++ src/main/java/com/onnoff/onnoff/domain/off/feedImage/controller/FeedImageController.java
@@ -5,6 +5,7 @@ import com.onnoff.onnoff.domain.off.feedImage.dto.FeedImageResponseDTO;
 import com.onnoff.onnoff.domain.off.feedImage.service.FeedImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +17,7 @@ public class FeedImageController {
 
     private final FeedImageService feedImageService;
 
-    @PostMapping("/feed-images")
+    @PostMapping(value = "/feed-images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "워라벨 피드 사진 업로드 API",description = "워라벨 피드에 사진을 업로드하는 API입니다. Query String으로 사용자 아이디와 사진 위치를 입력해 주세요.")
     public ApiResponse<FeedImageResponseDTO.ResultDTO> uploadFeedImage(@RequestParam(name = "userId") Long userId,
                                                                        @RequestParam(name = "location") Integer location,
@@ -30,7 +31,7 @@ public class FeedImageController {
         return ApiResponse.onSuccess(feedImageService.getFeedImage(userId));
     }
 
-    @PatchMapping("/feed-images/{feedImageId}")
+    @PatchMapping(value = "/feed-images/{feedImageId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "워라벨 피드 사진 수정 API",description = "워라벨 피드의 사진을 수정하는 API입니다.")
     public ApiResponse<FeedImageResponseDTO.ResultDTO> modifyFeedImage(@PathVariable(name = "feedImageId") Long feedImageId,
                                                                        @RequestPart(name = "image") MultipartFile multipartFile) {
