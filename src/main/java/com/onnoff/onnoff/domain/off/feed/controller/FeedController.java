433c15bb01f9c3cf11cac8e87855af542e06f65f
kimjm9841 <kimjm9841@naver.com>
2024-01-22 22:35:01 +0900

    feat: 피드 관련 컨트롤러 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/off/feed/controller/FeedController.java
@@ -0,0 +1,9 @@
+package com.onnoff.onnoff.domain.off.feed.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FeedController {
+}
