36b31da768d18772f049addf16c61c72162666b0
kimjm9841 <kimjm9841@naver.com>
2024-01-30 13:37:26 +0900

    feat: 요일 관련 컨트롤러 생성

---
+++ src/main/java/com/onnoff/onnoff/domain/weekday/controller/WeekdayController.java
@@ -0,0 +1,9 @@
+package com.onnoff.onnoff.domain.weekday.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WeekdayController {
+}
