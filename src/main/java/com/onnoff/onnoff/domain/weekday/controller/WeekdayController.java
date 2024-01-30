c138fd7c61271fd2bc4e8c1c20735ab809e8066b
kimjm9841 <kimjm9841@naver.com>
2024-01-30 15:15:22 +0900

    feat: 초기 요일 조회 API 구현

--- src/main/java/com/onnoff/onnoff/domain/weekday/controller/WeekdayController.java
+++ src/main/java/com/onnoff/onnoff/domain/weekday/controller/WeekdayController.java
@@ -1,9 +1,24 @@
 package com.onnoff.onnoff.domain.weekday.controller;
 
+import com.onnoff.onnoff.apiPayload.ApiResponse;
+import com.onnoff.onnoff.domain.weekday.dto.WeekdayResponseDTO;
+import com.onnoff.onnoff.domain.weekday.service.WeekdayService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
 public class WeekdayController {
+
+    private final WeekdayService weekdayService;
+
+    @GetMapping("/weekdays/init")
+    @Operation(summary = "초기 요일 조회 API", description = "오늘 날짜를 기준으로 일주일을 조회하는 API입니다.")
+    public ApiResponse<WeekdayResponseDTO.WeekdayResultDTO> getInitWeekday() {
+        return ApiResponse.onSuccess(weekdayService.getWeekday(LocalDate.now()));
+    }
 }
--- src/main/java/com/onnoff/onnoff/domain/weekday/service/WeekdayService.java
+++ src/main/java/com/onnoff/onnoff/domain/weekday/service/WeekdayService.java
@@ -1,4 +1,10 @@
 package com.onnoff.onnoff.domain.weekday.service;
 
+import com.onnoff.onnoff.domain.weekday.dto.WeekdayResponseDTO;
+
+import java.time.LocalDate;
+
 public interface WeekdayService {
+
+    WeekdayResponseDTO.WeekdayResultDTO getWeekday(LocalDate date);
 }
