bb4897311b589818fd96a7f2b7ca100b8e8386c3
realisshomyang <realisshoman@kakao.com>
2024-01-31 17:44:13 +0900

    [feat]: 푸시알람 분 단위 전송 기능 추가

--- src/main/java/com/onnoff/onnoff/domain/push/service/FcmSendNotificationService.java
+++ src/main/java/com/onnoff/onnoff/domain/push/service/FcmSendNotificationService.java
@@ -2,12 +2,19 @@ package com.onnoff.onnoff.domain.push.service;
 
 import com.google.firebase.messaging.Message;
 import com.onnoff.onnoff.domain.push.dto.NotificationMessage;
+import com.onnoff.onnoff.domain.user.User;
 import com.onnoff.onnoff.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class FcmSendNotificationService {
     private final FcmService fcmService;
@@ -17,11 +24,20 @@ public class FcmSendNotificationService {
                 NotificationMessage.toGoHomeNotificationTest();
         final Message message =
                     fcmService.createMessage("testtoken", notificationMessage);
-            fcmService.send(message);
+        fcmService.send(message);
     }
+    //1분 단위로 푸시알람 보내는 함수
     @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
     public void sendGoHomeNotificationByMinute() {
-        // Your logic to send the notification every minute
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalTime startTime = currentTime.minusSeconds(30).toLocalTime();
+        LocalTime endTime = currentTime.plusSeconds(30).toLocalTime();
+        List<User> userList = userRepository.findByPushNotificationTimeBetween(startTime, endTime);
+        for (User user : userList) {
+            NotificationMessage notificationMessage = NotificationMessage.toGoHomeNotification(user);
+            Message message = fcmService.createMessage(user.getFcmToken(), notificationMessage);
+            fcmService.send(message);
+        }
     }
 
 }
