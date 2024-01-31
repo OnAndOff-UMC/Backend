28bc4ca6ebcd39c45d342ad464e52186e9d3e5c3
wuseong <jws001228@naver.com>
2024-01-31 16:34:29 +0900

    fix: 로그인 응답 dto 및 컨버터 프론트 요청에 맞게 수정
    infoSet, accessToken, refreshToken 응답하는 걸로 변경

--- src/main/java/com/onnoff/onnoff/auth/dto/LoginRequestDTO.java
+++ src/main/java/com/onnoff/onnoff/auth/dto/LoginRequestDTO.java
@@ -5,22 +5,21 @@ import lombok.Getter;
 
 public class LoginRequestDTO {
     @Getter
+    public static class ServerTokenValidateDTO{
+        private String accessToken;
+        private String refreshToken;
+    }
+    @Getter
     public static class AppleTokenValidateDTO{
-        @JsonProperty("user")
         private String oauthId;
-        @JsonProperty("full_name")
         private String fullName;
         private String email;
-        @JsonProperty("identity_token")
         private String identityToken;
-        @JsonProperty("authorization_code")
         private String authorizationCode;
     }
     @Getter
     public static class KakaoTokenValidateDTO{
-        @JsonProperty("identity_token")
         private String identityToken;
-        @JsonProperty("access_token")
         private String accessToken;
     }
 }
