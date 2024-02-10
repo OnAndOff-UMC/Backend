73cbb74583329b9b860a28d43dfb19b7a599a907
wuseong <jws001228@naver.com>
2024-02-10 22:40:12 +0900

    feat: 애플 auth client 토큰 폐기 요청 수정

--- src/main/java/com/onnoff/onnoff/auth/feignClient/client/AppleAuthClient.java
+++ src/main/java/com/onnoff/onnoff/auth/feignClient/client/AppleAuthClient.java
@@ -5,8 +5,6 @@ import com.onnoff.onnoff.auth.feignClient.config.FeignConfig;
 import com.onnoff.onnoff.auth.feignClient.dto.JwkResponse;
 import com.onnoff.onnoff.auth.feignClient.dto.TokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +20,6 @@ public interface AppleAuthClient{
     TokenResponse getToken(@RequestBody Map<String, ?> requestBody);
 
     //회원 탈퇴 메서드
-    @GetMapping("/oauth2/v2/revoke")
+    @PostMapping(value ="/oauth2/v2/revoke", consumes = "application/x-www-form-urlencoded")
     void revokeTokens(@RequestBody Map<String, ?> requestBody);
 }
