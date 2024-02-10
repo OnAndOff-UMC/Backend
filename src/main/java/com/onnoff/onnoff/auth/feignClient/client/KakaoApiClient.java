4781b876ccd50100164fdb3f0f87025d577ed3d0
wuseong <jws001228@naver.com>
2024-02-10 21:16:46 +0900

    feat: 카카오 api 클라이언트 연결끊기 요청 추가

--- src/main/java/com/onnoff/onnoff/auth/feignClient/client/KakaoApiClient.java
+++ src/main/java/com/onnoff/onnoff/auth/feignClient/client/KakaoApiClient.java
@@ -3,7 +3,10 @@ package com.onnoff.onnoff.auth.feignClient.client;
 import com.onnoff.onnoff.auth.feignClient.config.FeignConfig;
 import com.onnoff.onnoff.auth.feignClient.dto.kakao.KakaoOauth2DTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /*
  토큰 유효성 검증 하고 사용자 정보 가져오는 client
@@ -15,5 +18,7 @@ public interface KakaoApiClient {
     KakaoOauth2DTO.TokenValidateResponseDTO getTokenValidate(@RequestHeader("Authorization") String accessToken);
     @GetMapping(value = "/v1/oidc/userinfo")
     KakaoOauth2DTO.UserInfoResponseDTO getUserInfo(@RequestHeader("Authorization") String accessToken);
+    @PostMapping(value = "/v1/user/unlink", consumes = "application/x-www-form-urlencoded")
+    ResponseEntity unlink(@RequestHeader("Authorization") String adminKey, @RequestBody Map<String, ?> requestBody);
 
 }
